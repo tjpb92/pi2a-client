d53ec163eb0f83ed1d2d1d5d12ac92bbf1aaf805
thierry.baribaud <thierry.baribaud@gmail.com>
2023-08-28 17:07:08 +0200

    Evolution au 11/08/2023, ANLTXTB, version 0.32.7
    - Création du programme ProviderIsGoingCheckedTest.java de test du 
      correctif pour la détection de l’événement ProviderIsGoingChecked.
    - Création du programme ProviderContactedCheckedTest.java de test du 
      correctif pour la détection de l’événement ProviderContactedChecked.
    - Mise à jour de ClientSuite.java en conséquence.

--- test/pi2a/client/ClientSuite.java
+++ test/pi2a/client/ClientSuite.java
@@ -9,14 +9,16 @@ import org.junit.runners.Suite;
  * Jeux de tests pour tester toutes les classes du projet
  *
  * @author Thierry Baribaud
- * @version 0.32.6
+ * @version 0.32.7
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     Bug190925Test.class,
     Bug230728Test.class,
     DissociateProviderContactFromPatrimonyTest.class,
-    NotifyRepairRequestByMail_bug230321Test.class
+    NotifyRepairRequestByMail_bug230321Test.class,
+    ProviderIsGoingCheckedTest.class,
+    ProviderContactedCheckedTest.class
 })
 public class ClientSuite {
 
---
+++ test/pi2a/client/ProviderContactedCheckedTest.java
@@ -0,0 +1,179 @@
+package pi2a.client;
+
+import bkgpi2a.Event;
+import bkgpi2a.EventList;
+import bkgpi2a.HttpsClient;
+import bkgpi2a.Identifiants;
+import bkgpi2a.Range;
+import bkgpi2a.TicketEventResultView;
+import bkgpi2a.WebServer;
+import bkgpi2a.WebServerException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import utils.ApplicationProperties;
+import com.anstel.ticketEvents.ProviderContactedChecked;
+
+/**
+ * Programme permettant de tester le comportement du programme d'extraction
+ * d'événements dans le cas du bug du 7 août 2023 avec la non détection de
+ * l'événnement ProviderContactedChecked.
+ *
+ * @author Thierry Baribaud
+ * @version 0.32.7
+ */
+public class ProviderContactedCheckedTest {
+
+    /**
+     * Common Jackson object mapper
+     */
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public ProviderContactedCheckedTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Programme permettant de tester le comportement du programme d'extraction
+     * d'événements dans le cas du bug du 7 août 2023 avec la non détection de
+     * l'événnement ProviderContactedChecked.
+     */
+    @Test
+    public void testProviderContactedChecked() {
+        String[] args = {"-webserver", "prod2", "-dbserver", "pre-prod", "-d"};
+        GetArgs getArgs;
+        WebServer webServer;
+        ApplicationProperties applicationProperties;
+        boolean debugMode;
+        String value;
+        Identifiants identifiants = new Identifiants();
+        HttpsClient httpsClient;
+        String from;
+        String to;
+        String baseCommand;
+        Range range;
+        StringBuffer command;
+        int responseCode;
+        String response;
+        TicketEventResultView ticketEventResultView;
+        EventList events;
+//        JsonString jsonString;
+//        String json;
+        int i;
+        ProviderContactedChecked providerContactedChecked;
+
+        System.out.println("ProviderContactedCheckedTest");
+
+        try {
+            System.out.println("Analyse des arguments de la ligne de commande ...");
+            getArgs = new GetArgs(args);
+            System.out.println(getArgs);
+            assertNotNull(getArgs);
+            debugMode = getArgs.getDebugMode();
+
+            System.out.println("Lecture des paramètres d'exécution ...");
+            applicationProperties = new ApplicationProperties("pi2a-client.prop");
+
+            System.out.println("Lecture des paramètres du serveur Web ...");
+            webServer = new WebServer(getArgs.getWebServerType(), applicationProperties);
+            if (debugMode) {
+                System.out.println(webServer);
+            }
+
+            value = applicationProperties.getProperty(getArgs.getWebServerType() + ".webserver.login");
+            if (value != null) {
+                identifiants.setLogin(value);
+            } else {
+                throw new WebServerException("Nom utilisateur pour l'accès Web non défini");
+            }
+
+            value = applicationProperties.getProperty(getArgs.getWebServerType() + ".webserver.passwd");
+            if (value != null) {
+                identifiants.setPassword(value);
+            } else {
+                throw new WebServerException("Mot de passe pour l'accès Web non défini");
+            }
+            if (debugMode) {
+                System.out.println(identifiants);
+            }
+
+            System.out.println("Ouverture de la connexion au site Web : " + webServer.getName());
+            httpsClient = new HttpsClient(webServer.getIpAddress(), identifiants, debugMode, false);
+
+            System.out.println("Authentification en cours ...");
+            httpsClient.sendPost(HttpsClient.REST_API_PATH + HttpsClient.LOGIN_CMDE);
+
+            // from=2023-08-07T10:19:41Z to=2023-08-07T10:20:46Z
+            from = "2023-08-07T10:19:41Z";
+            to = "2023-08-07T10:20:46Z";
+            baseCommand = HttpsClient.EVENT_API_PATH + HttpsClient.TICKETS_CMDE;
+            if (debugMode) {
+                System.out.println("  Commande pour récupérer les événements : " + baseCommand);
+            }
+            range = new Range();
+//            range.setOffset(0);
+            range.setLimit(17);
+            range.setCount(18);
+            command = new StringBuffer(baseCommand);
+            command.append("?range=").append(range.getRange()).append("&");
+            command.append("from=").append(from);
+            command.append("&to=").append(to);
+            if (debugMode) {
+                System.out.println("  Commande et arguments pour récupérer les événements : " + command);
+            }
+            httpsClient.sendGet(command.toString());
+            responseCode = httpsClient.getResponseCode();
+            System.out.println("  responseCode:" + responseCode);
+            response = httpsClient.getResponse();
+            System.out.println("  response:" + response);
+            ticketEventResultView = objectMapper.readValue(response, TicketEventResultView.class);
+            events = ticketEventResultView.getEvents();
+            System.out.println("  nb event(s):" + events.size());
+            i = 0;
+            for (Event event : events) {
+                i++;
+                System.out.println("  event(" + i + "), processUid:" + event.getProcessUid() + ", date:" + event.getDate() + ", eventType:" + event.getEventType());
+                if (event instanceof ProviderContactedChecked) {
+                    providerContactedChecked = (ProviderContactedChecked) event;
+                    System.out.println("    " + providerContactedChecked);
+                }
+            }
+
+            // Ce qui suit est bugué !!!
+//            jsonString = new JsonString(response);
+//            i=0;
+//            while ((json = jsonString.next()) != null) {
+//                i++;
+//                System.out.println("  json(" + i + "):" + json);
+//            }
+        } catch (utils.GetArgsException | IOException | WebServerException ex) {
+            Logger.getLogger(ProviderContactedCheckedTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail(ex.getMessage());
+        } catch (Exception ex) {
+            Logger.getLogger(ProviderContactedCheckedTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
