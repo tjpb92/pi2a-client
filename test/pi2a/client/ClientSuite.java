d5399ec1147cbbd1c018788679363df4162420fa
thierry.baribaud <thierry.baribaud@gmail.com>
2023-10-30 18:18:17 +0100

    Evolution au 30/10/2023, ANLTXTB, version 0.32.10
    - Création du programme ContactNotifiedOfSupportTest.java de test du 
      correctif pour la détection de l’événement ContactNotifiedOfSupport.
    - Le test unitaire passe.
    - Mise à jour de ClientSuite.java en conséquence.
    - On introduit dans ContactNotifiedOfSupport.java la référence à l’objet 
      Persona et on teste. Le test passe.
    - Même opération pour InterventionReportGottenFromProvider, résultat : OK.
    - Même opération pour InterventionReportGottenFromResident, résultat : OK.
    - Même opération pour ProviderContactedChecked, résultat : OK.
    - Même opération pour ProviderIsGoingChecked, résultat : OK.
    - Voir NotificationDecorator.scala pour les commentaires sur les 
      événements.

--- test/pi2a/client/ClientSuite.java
+++ test/pi2a/client/ClientSuite.java
@@ -9,12 +9,13 @@ import org.junit.runners.Suite;
  * Jeux de tests pour tester toutes les classes du projet
  *
  * @author Thierry Baribaud
- * @version 0.32.9
+ * @version 0.32.10
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     Bug190925Test.class,
     Bug230728Test.class,
+    ContactNotifiedOfSupportTest.class,
     ContactToCallbackCorrectedTest.class,
     DissociateProviderContactFromPatrimonyTest.class,
     InterventionReportGottenFromProviderTest.class,
